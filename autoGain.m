cf0b23186111388fb7caaa7c645e139506f56276
DK5 <d.karasikov@gmail.com>
2015-07-13 20:18:21 +0300

    few fixes...

--- autoGain.m
+++ autoGain.m
@@ -1,6 +1,6 @@
-function autoGain( obj1 )
+function autoGain( obj1, tnum )
 %autoGain gets a measure from the lock-in and sets the proper sensetivity
-%   ampl is the measurement
+%   tnum is the trace number
 %   obj1 is the Lock-in object
 
 level = [2e-9,5e-9,10e-9,20e-9,50e-9,100e-9,200e-9,500e-9...
@@ -8,15 +8,19 @@ level = [2e-9,5e-9,10e-9,20e-9,50e-9,100e-9,200e-9,500e-9...
     ,1e-3,2e-3,5e-3,10e-3,20e-3,50e-3,100e-3,200e-3,500e-3,1];
 scale = 0.9*level;
 
-ampl = str2double(query(obj1, 'outp? 4'));  % get data
+ampl = askTrace(obj1, tnum);  % get data
 
 logic = ampl<scale;     % find scale
 [~,ind] = max(logic);   % proper scale index is the first bigger
 
 clevel = str2double(query(obj1, 'sens?'))+1; % read the sensitivity
 
-if clevel<ind   % if not at the proper scale
-    fprintf(obj1, ['sens ',num2str(ind-1)]);
-    autoGain(obj1); % check scaling again
+if clevel~=ind   % if not at the proper scale
+    try
+        fprintf(obj1, ['sens ',num2str(ind-1)]);
+    catch 
+        error('Error setting sensitivity');
+    end   
+    autoGain(obj1,tnum);  % keep tuning till the proper sensitivity
 end
 
--- quietFREQ.m
+++ quietFREQ.m
@@ -5,26 +5,29 @@ lk = GPcon(2);  % lock-in
 fg = GPcon(25); % function generator
 
 setTrace(lk,1,7,0,0,0); % set trace 1 as Rn = magnitude noise
-refSRC(lk,'external');  % set reference source to external
-ampl(fg,1);             % set amplitude to 1V
-output(fg,'on50');      % set output to 50 ohm
+setTrace(lk,2,3,0,0,0); % set trace 1 as R = magnitude
+refSRC(lk,'internal');  % set reference source to external
+%ampl(fg,1);             % set amplitude to 1V
+%output(fg,'on50');      % set output to 50 ohm
 sampleRate(lk,'max');   % set sample rate to max
 
 % data arrays
-data = zeros(489,50);
+data = zeros(239,10);
 
 % Scanning frequencies
-for FREQ = 25:2:1001
-    freq(fg,FREQ);
-    execute(fg);
+for FREQ = 25:2:501
+    freq(lk,FREQ);
+%    execute(fg);
     pause(0.5);
-    for k = 1:50
+    for k = 1:10
         line = 0.5*(FREQ-25+2);
-        data(line,k) = askTrace(lk,1);
+        autoGain(lk,2);
+        data(line,k) = askTrace(lk,2);
         pause(0.001);
     end
 end
 
-avg = mean(data,2);          % standard deviation
-[minVal, ind] = min(avg); % get minimum std and its index
+avg = mean(data,2);          
+S = std(data,0,2);          % standard deviation
+[minVal, ind] = min(avg);   % get minimum std and its index
 fbest = 25+2*(ind-1);       % best frequency
