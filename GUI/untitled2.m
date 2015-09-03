a24064815f655b1c22bd3ca1936b4ce51b8fe4d4
ScubaDiving <amirmel@me.com>
2015-09-03 15:49:33 +0300

    gui progress
    amir is the king

---
+++ GUI/untitled2.m
@@ -0,0 +1,505 @@
+function varargout = untitled2(varargin)
+% UNTITLED2 M-file for untitled2.fig
+%      UNTITLED2, by itself, creates a new UNTITLED2 or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED2 returns the handle to a new UNTITLED2 or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED2.M with the given input arguments.
+%
+%      UNTITLED2('Property','Value',...) creates a new UNTITLED2 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled2_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled2_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled2
+
+% Last Modified by GUIDE v2.5 03-Sep-2015 12:19:20
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled2_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled2_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled2 is made visible.
+function untitled2_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled2 (see VARARGIN)
+
+% Choose default command line output for untitled2
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled2 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled2_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in Set_Field.
+function Set_Field_Callback(hObject, eventdata, handles)
+% hObject    handle to Set_Field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+          
+
+Destination_field = get(handles.txt_destination_field, 'String');
+index_selected = get(handles.listbox1, 'Value');
+
+%index_selected = get(handles.listbox1, 'Value');
+
+%list = get(handles.listbox1,'String'); %Get the cell array
+%item_selected = list{index_selected}; 
+
+
+
+
+add_item_to_list_box(handles.listbox1, ['Set field ',Destination_field, ' [Oe]' ], index_selected);
+
+
+
+
+
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function txt_destination_field_Callback(hObject, eventdata, handles)
+% hObject    handle to txt_destination_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txt_destination_field as text
+%        str2double(get(hObject,'String')) returns contents of txt_destination_field as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txt_destination_field_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txt_destination_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in Set_scan_field.
+function Set_scan_field_Callback(hObject, eventdata, handles)
+% hObject    handle to Set_scan_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+Start_Field = get(handles.txt_Start_field, 'String');
+Stop_Field = get(handles.txt_Stop_field, 'String');
+Num_of_steps = get(handles.txt_field_number_of_steps, 'String');
+
+index_selected = get(handles.listbox1, 'Value');
+add_item_to_list_box(handles.listbox1, 'End' , index_selected);
+add_item_to_list_box(handles.listbox1, ['Scan field from ',Start_Field, ' [Oe] to ', Stop_Field, ' [Oe] in ', Num_of_steps, ' steps'], index_selected);
+%add_item_to_list_box(handles.listbox1, 'End' , index_selected+1);
+
+
+function txt_Start_field_Callback(hObject, eventdata, handles)
+% hObject    handle to txt_Start_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txt_Start_field as text
+%        str2double(get(hObject,'String')) returns contents of txt_Start_field as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txt_Start_field_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txt_Start_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function txt_Stop_field_Callback(hObject, eventdata, handles)
+% hObject    handle to txt_Stop_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txt_Stop_field as text
+%        str2double(get(hObject,'String')) returns contents of txt_Stop_field as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txt_Stop_field_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txt_Stop_field (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function txt_field_number_of_steps_Callback(hObject, eventdata, handles)
+% hObject    handle to txt_field_number_of_steps (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txt_field_number_of_steps as text
+%        str2double(get(hObject,'String')) returns contents of txt_field_number_of_steps as a double
+
+%scan field step size check
+Start_Field = str2num(get(handles.txt_Start_field, 'String'));
+Stop_Field = str2num(get(handles.txt_Stop_field, 'String'));
+Num_of_steps = str2num(get(handles.txt_field_number_of_steps, 'String'));
+if(((Stop_Field-Start_Field)/Num_of_steps)<=10.0)
+    errordlg('Each step must be bigger than 10!','Error 0x001')
+    min_steps= (Stop_Field-Start_Field)/10; %minimum num of steps possible
+    set(handles.txt_field_number_of_steps,'String', num2str(min_steps));
+    return
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function txt_field_number_of_steps_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txt_field_number_of_steps (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in Set_take_picture.
+function Set_take_picture_Callback(hObject, eventdata, handles)
+% hObject    handle to Set_take_picture (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+index_selected = get(handles.listbox1, 'Value')
+add_item_to_list_box(handles.listbox1, 'moty',index_selected);
+
+function h = add_item_to_list_box_end(h, newitem)
+% ADDITEMTOLISTBOX - add a new items to the listbox
+% H = ADDITEMTOLISTBOX(H, STRING)
+% H listbox handle
+% STRING a new item to display
+
+    oldstring = get(h, 'string');
+    if isempty(oldstring)
+        newstring = newitem;
+     elseif ~iscell(oldstring)
+         newstring = {oldstring newitem};
+    else
+        newstring = {oldstring{:} newitem};
+    end
+    set(h, 'string', newstring);
+
+function h = add_item_to_list_box(h, newitem, index)
+% ADDITEMTOLISTBOX - add a new items to the listbox
+% H = ADDITEMTOLISTBOX(H, STRING)
+% H listbox handle
+% STRING a new item to display
+
+    oldstring = get(h, 'string'); %Get the cell array from the listbox. 
+    
+    space = 0;
+    for i=1:(index-1) %%Move right the text according to the loops
+        item_selected = oldstring{i};
+        L=length(item_selected);
+        if L>(4+space)
+           if strcmp(item_selected((1+space):(4+space)), 'Scan')
+             space = space +3;
+           end
+        elseif L==(3+space)
+           if strcmp(item_selected((1+space):(3+space)), 'End')
+             space = space -3;
+           end
+        end
+   end
+    
+    for j=1:space
+        newitem = [' ', newitem];
+    end
+    
+    
+    
+    if isempty(oldstring)
+        newstring = {newitem};
+%     elseif ~iscell(oldstring)
+%          newstring = {oldstring newitem};
+    else
+        newstring = {oldstring{1:(index-1)}   newitem     oldstring{index:end}}; %Create the new cell array for the list box
+    end
+    set(h, 'string', newstring); %Set the new cell array to the list box
+
+function h = del_item_from_list_box(h, index)
+% del_item_from_list_box - add a new items to the listbox
+% H = del_item_from_list_box(H, index)
+% H listbox handle
+% index - index to remove (?)
+% STRING a new item to display
+
+    oldstring = get(h, 'string');
+    L=length(oldstring);
+    if isempty(oldstring)
+    elseif index==L 
+    else
+        newstring = {oldstring{1:(index-1)} oldstring{index+1:end}};
+        set(h, 'string', newstring);
+    end
+        
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+set(hObject, 'string', {'End sequence'});
+
+
+
+% --- Executes on button press in Set_wait.
+function Set_wait_Callback(hObject, eventdata, handles)
+% hObject    handle to Set_wait (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+Waiting_time = get(handles.txt_waiting_time, 'String');
+index_selected = get(handles.listbox1, 'Value');
+add_item_to_list_box(handles.listbox1, ['Wait ',Waiting_time, ' [sec]' ], index_selected);
+
+
+
+
+function txt_waiting_time_Callback(hObject, eventdata, handles)
+% hObject    handle to txt_waiting_time (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txt_waiting_time as text
+%        str2double(get(hObject,'String')) returns contents of txt_waiting_time as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txt_waiting_time_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txt_waiting_time (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in btn_Set_del_line.
+function btn_Set_del_line_Callback(hObject, eventdata, handles)
+% hObject    handle to btn_Set_del_line (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+index_selected = get(handles.listbox1, 'Value')
+
+del_item_from_list_box(handles.listbox1,index_selected);
+
+
+
+function edit9_Callback(hObject, eventdata, handles)
+% hObject    handle to edit9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit9 as text
+%        str2double(get(hObject,'String')) returns contents of edit9 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit9_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in btn_set_remark.
+function btn_set_remark_Callback(hObject, eventdata, handles)
+% hObject    handle to btn_set_remark (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+Remark = get(handles.txt_remark, 'String');
+index_selected = get(handles.listbox1, 'Value');
+
+list = get(handles.listbox1,'String');
+item_selected = list{index_selected} 
+
+
+add_item_to_list_box(handles.listbox1, ['###  ', Remark, '  ###' ], index_selected);
+
+
+function txt_remark_Callback(hObject, eventdata, handles)
+% hObject    handle to txt_remark (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txt_remark as text
+%        str2double(get(hObject,'String')) returns contents of txt_remark as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txt_remark_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txt_remark (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in Reset_button.
+function Reset_button_Callback(hObject, eventdata, handles)
+% hObject    handle to Reset_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+initialize_gui(gcbf, handles, true);
+
+function initialize_gui(hObject, handles, isreset)
+% function used to reset all properies of gui
+
+set(handles.txt_destination_field , 'String','0');    %Reset DESTINATION field
+
+set(handles.txt_Start_field,'String','0');
+set(handles.txt_Stop_field,'String','100');  %Reset all SCAN fields
+set(handles.txt_field_number_of_steps,'String','10');
+
+set(handles.txt_remark,'String','');   %Reset REMARK Field
+
+set(handles.txt_waiting_time,'String','1');    %Reset WAIT TIME field
+
+set(handles.listbox1, 'String', {'End sequence'});  %Reset LISTBOX
+% Update handles structure
+guidata(handles.figure1, handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function uipanel10_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to uipanel10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes on button press in write_to_file_button.
+function write_to_file_button_Callback(hObject, eventdata, handles)
+% hObject    handle to write_to_file_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+contents = get(handles.listbox1,'String') %extract all contents of listbox as cell array
+
+FID = fopen('commands.m','w');
+formatSpec = '%s\r\n';
+[nrows,ncols] = size(contents);
+for row = 1:nrows
+    fprintf(FID,formatSpec,contents{row,:});
+end
+
+fclose(FID);
+type commands.m
+
