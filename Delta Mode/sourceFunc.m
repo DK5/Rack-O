99fdb80c8b77e3f88e538d4e2de24b81906a00a9
OziKashvil <oz.karasikov@gmail.com>
2015-09-24 16:15:00 +0300

    More Delta-Mode Functions
    Compliance & Source functions added.

---
+++ Delta Mode/sourceFunc.m
@@ -0,0 +1,14 @@
+function sourceFunc( cs_obj , func)
+% sourceFunc specifies source function - Voltage or Current
+%   func - c = Current ; v = Voltage
+
+switch func
+    case 'c'
+        str = 'CURR';
+    case 'v'
+        str = 'VOLT';
+end
+
+fprintf(cs_obj,':SOURce:FUNCtion ',str);    % Select SOURce Mode
+
+end
