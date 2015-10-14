f27aa1969b1df0053bffc978599af034cb141f39
DK5 <d.karasikov@gmail.com>
2015-10-14 14:47:00 +0300

    I&V source

---
+++ SourceMeter/current.m
@@ -0,0 +1,12 @@
+function current( cs_obj , uamps, state )
+%current( cs_obj , uamps ) supplies current from source meter
+%   cs_obj = current source object
+%   uamps = current level in uAmpere
+
+fprintf(cs_obj,[':OUTPut ', state]);   % Turn source on/off
+sourceFunc(cs_obj, 'c');
+amps = uamps*10^-6;
+fprintf(cs_obj, [':source:current ',num2str(amps)]);
+
+end
+
---
+++ SourceMeter/voltage.m
@@ -0,0 +1,11 @@
+function voltage( vs_obj , uVolts, state )
+%voltage( vs_obj , uamps ) supplies voltage from source meter
+%   vs_obj = voltage source object
+%   uVolts = voltage level in uVolts
+
+fprintf(cs_obj,[':OUTPut ', state]);   % Turn source on/off
+sourceFunc(vs_obj, 'v');
+volts = uVolts*10^-6;
+fprintf(vs_obj, [':source:voltage ',num2str(volts)]);
+
+end
